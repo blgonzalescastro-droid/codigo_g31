7eb8bc1842d69b1f8e214d2f7f27da96d2411816
blgonzalescastro-droid <blgonzalescastro@gmail.com>
2026-05-13 21:44:13 -0500

    first commit

---
+++ Semana01/Clase01/02_sintaxis.py
@@ -0,0 +1,8 @@
+def suma(a,b):
+    return a + b
+
+numero=20
+
+if numero > 10:
+    resultado = suma(5,2)
+    print(f"El resultado de la suma es: {resultado}")
---
+++ Semana01/Clase01/03_tipos_de_datos.py
@@ -0,0 +1,11 @@
+print(type(10))  # int
+print(type(3.14))  # float
+print(type("Hola"))  # str
+print(type(True))  # bool
+print(type(None))  # NoneType
+
+variable = 42
+variable = "Ahora soy una cadena"
+variable = 3.14
+
+print(variable)  # Imprime el valor actual de la variable
